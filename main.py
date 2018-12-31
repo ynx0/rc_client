128b0332ca17a0e7ccda65f9477adbad19abfdca
Yaseen Shaikh <yaseen.nvt@gmail.com>
2018-12-31 11:53:12 -0500

    more keyboard stuff that was left out

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from drivers import xbox
 from drivers.keyboard.KeyboardDriver import KeyboardDriver
-from drivers.keyboard.KeyboardUtils import KeyControls, KeyList
-
-Arrows = KeyList.Arrows  # todo FIXME bro
+from drivers.keyboard.KeyboardUtils import KeyControls, KeyList, Arrows
 
 default_keymap = {
     KeyControls.STOP: [b's'],
@@ -17,12 +15,25 @@ default_keymap = {
     KeyControls.SET_SPEED_2: [b'2'],
 }
 
+alt_keymap = {
+    KeyControls.STOP: [b's'],
+    KeyControls.EXIT: [b'e', KeyList.ESC, KeyList.CTRLC],
+    # directions
+    KeyControls.UP: [b'i'],
+    KeyControls.DOWN: [b','],
+    KeyControls.LEFT: [b'j'],
+    KeyControls.RIGHT: [b'l'],
+    # speed controls
+    KeyControls.SET_SPEED_1: [b'1'],
+    KeyControls.SET_SPEED_2: [b'2'],
+}
+
 
 # todo iterate through directories and programmatically create this list
 def main():
     print("Which input method would you like to activate")
     print("1 - Keyboard")
-    # print("2 - Keyboard Alt")  # todo implement a keymap for it later
+    print("2 - Keyboard Alt")  # todo implement a keymap for it later
     print("3 - Xbox")
     choice = input("Choose a number -->")
 
@@ -30,9 +41,10 @@ def main():
         print("Starting Driver Keyboard CLI ")
         kbd_cli = KeyboardDriver("Keyboard Driver", "kbd_drv00", default_keymap)
         kbd_cli.start()
-    # elif choice == "2":
-    #     print("Starting alt driver cli")
-    #     keyboard_cli_alt.start()
+    elif choice == "2":
+        print("Starting alt driver cli")
+        kbd_alt = KeyboardDriver("Keyboard Driver Alt", "kbd_drv01_alt", alt_keymap)
+        kbd_alt.start()
     elif choice == "3":
         print("Starting Xbox Controller")
         xbox.start()
